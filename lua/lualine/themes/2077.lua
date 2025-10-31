e8b0465441cc16c0317ebc92070023bfe99f64a3
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-10-31 08:33:09 +0100

    refactor(master): Simplify color definitions in 2077 theme by removing unnecessary double colons and duplicate code

--- lua/lualine/themes/2077.lua
+++ lua/lualine/themes/2077.lua
@@ -3,59 +3,57 @@
 -- Credit: Zoltan Dalmadi(lightline)
 -- LuaFormatter off
 local colors = {
-<<<<<<< HEAD
-  blue = "#03d8f3",   -- Insert mode blue
-  green = "#00ffc8",  -- Command mode green
-  purple = "#ff0055", -- (Not used, but available for future)
-  red1 = "#ff007c",   -- Normal mode pink:
-  yellow = "#ffed00", -- Visual mode yellow
-  orange = "#ffb800", -- (Not used, but available for future)
-  fg = "#e2dddf",     -- Foreground color
-  fg2 = "#12231c",    -- Foreground color
-  bg = "#353843",     -- Background color
-  gray1 = "#2f404d",  -- Inactive gray1
-  gray2 = "#3D898d",  -- Inactive gray2
-  gray3 = "#3e4452",  -- Inactive gray3
+	blue = "#03d8f3", -- Insert mode blue
+	green = "#00ffc8", -- Command mode green
+	purple = "#ff0055", -- (Not used, but available for future)
+	red1 = "#ff007c", -- Normal mode pink:
+	yellow = "#ffed00", -- Visual mode yellow
+	orange = "#ffb800", -- (Not used, but available for future)
+	fg = "#e2dddf", -- Foreground color
+	fg2 = "#12231c", -- Foreground color
+	bg = "#353843", -- Background color
+	gray1 = "#2f404d", -- Inactive gray1
+	gray2 = "#3D898d", -- Inactive gray2
+	gray3 = "#3e4452", -- Inactive gray3
 }
 
-local theme =  {
-  normal = {
-    a = { fg = colors.fg2, bg = colors.red1, gui = "bold", },
-    b = { fg = colors.fg, bg = colors.gray3 },
-    c = { fg = colors.fg2, bg = colors.red1 },
-    x = { fg = colors.fg, bg = colors.gray1 },
-
-  },
-  insert = {
-    a = { fg = colors.fg2, bg = colors.blue, gui = "bold" },
-    b = { fg = colors.fg, bg = colors.gray3 },
-    c = { fg = colors.fg2, bg = colors.blue },
-    x = { fg = colors.fg, bg = colors.gray1 },
-  },
-  visual = {
-    a = { fg = colors.fg2, bg = colors.yellow, gui = "bold" },
-    b = { fg = colors.fg, bg = colors.gray3 },
-    c = { fg = colors.fg2, bg = colors.yellow },
-    x = { fg = colors.fg, bg = colors.gray1 },
-  },
-  replace = {
-    a = { fg = colors.fg2, bg = colors.red1, gui = "bold" },
-    b = { fg = colors.fg, bg = colors.gray3 },
-    c = { fg = colors.fg2, bg = colors.red1 },
-    x = { fg = colors.fg, bg = colors.gray1 },
-  },
-  command = {
-    a = { fg = colors.fg2, bg = colors.green, gui = "bold" },
-    b = { fg = colors.fg, bg = colors.gray3 },
-    c = { fg = colors.fg2, bg = colors.green },
-    x = { fg = colors.fg, bg = colors.gray1 },
-  },
-  inactive = {
-    a = { fg = colors.fg, bg = colors.gray1 },
-    b = { fg = colors.fg, bg = colors.gray1 },
-    c = { fg = colors.fg, bg = colors.gray1 },
-    x = { fg = colors.fg, bg = colors.gray1 },
-  },
+local theme = {
+	normal = {
+		a = { fg = colors.fg2, bg = colors.red1, gui = "bold" },
+		b = { fg = colors.fg, bg = colors.gray3 },
+		c = { fg = colors.fg2, bg = colors.red1 },
+		x = { fg = colors.fg, bg = colors.gray1 },
+	},
+	insert = {
+		a = { fg = colors.fg2, bg = colors.blue, gui = "bold" },
+		b = { fg = colors.fg, bg = colors.gray3 },
+		c = { fg = colors.fg2, bg = colors.blue },
+		x = { fg = colors.fg, bg = colors.gray1 },
+	},
+	visual = {
+		a = { fg = colors.fg2, bg = colors.yellow, gui = "bold" },
+		b = { fg = colors.fg, bg = colors.gray3 },
+		c = { fg = colors.fg2, bg = colors.yellow },
+		x = { fg = colors.fg, bg = colors.gray1 },
+	},
+	replace = {
+		a = { fg = colors.fg2, bg = colors.red1, gui = "bold" },
+		b = { fg = colors.fg, bg = colors.gray3 },
+		c = { fg = colors.fg2, bg = colors.red1 },
+		x = { fg = colors.fg, bg = colors.gray1 },
+	},
+	command = {
+		a = { fg = colors.fg2, bg = colors.green, gui = "bold" },
+		b = { fg = colors.fg, bg = colors.gray3 },
+		c = { fg = colors.fg2, bg = colors.green },
+		x = { fg = colors.fg, bg = colors.gray1 },
+	},
+	inactive = {
+		a = { fg = colors.fg, bg = colors.gray1 },
+		b = { fg = colors.fg, bg = colors.gray1 },
+		c = { fg = colors.fg, bg = colors.gray1 },
+		x = { fg = colors.fg, bg = colors.gray1 },
+	},
 }
 
 -- Added some default configuration for lualine
@@ -78,7 +76,7 @@ local theme =  {
   },
   tabline = {
       lualine_a = {
-          {'buffers', 
+          {'buffers',
       use_mode_colors = true
       }
           },
@@ -91,38 +89,3 @@ local theme =  {
 }--]]
 
 return theme
-=======
-  blue = "#03d8f3",
-  green = "#00ffc8",
-  purple = "#ff0055",
-  red1 = "#ff007c",
-  yellow = "#FCEE0C",
-  orange = "#ffb800",
-  fg = "#353843",
-  -- fg = "#e2dddf",
-  bg = "#ff007c",
-  -- bg = "#00ffc8",
-  -- bg = "#FCEE0C",
-  -- bg = "#03d8f3",
-  gray1 = "#2f404d",
-  gray2 = "#3D898d",
-  gray3 = "#3e4452",
-}
--- LuaFormatter on
-return {
-  normal = {
-    a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
-    b = { fg = colors.fg, bg = colors.bg },
-    c = { fg = colors.fg, bg = colors.bg },
-  },
-  insert = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  visual = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  command = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  replace = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  inactive = {
-    a = { fg = colors.fg, bg = colors.bg },
-    b = { fg = colors.fg, bg = colors.bg },
-    c = { fg = colors.fg, bg = colors.bg },
-  },
-}
->>>>>>> ac84f753d225649c95ea86b0157f982c8f694237
